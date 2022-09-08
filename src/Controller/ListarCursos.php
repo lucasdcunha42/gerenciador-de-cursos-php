b2376b973a88ace110cdba9dd08a33301ccd2c95
lucasdcunha42 <lucasdcunha42@gmail.com>
2022-09-08 11:22:45 -0300

    Initial commit

---
+++ src/Controller/ListarCursos.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Alura\Cursos\Controller;
+
+use Alura\Cursos\Entity\Curso;
+use Alura\Cursos\Infra\EntityManagerCreator;
+
+class ListarCursos extends ControllerComHtml implements InterfaceControladorRequisicao
+{
+    /**
+     * @var \Doctrine\ORM\EntityRepository
+     */
+    private $repositorioDeCursos;
+
+    public function __construct()
+    {
+        $entityManager = (new EntityManagerCreator())
+            ->getEntityManager();
+        $this->repositorioDeCursos = $entityManager
+            ->getRepository(Curso::class);
+    }
+
+    public function processaRequisicao(): void
+    {
+        echo $this->renderizaHtml('cursos/listar-cursos.php', [
+            'cursos' => $this->repositorioDeCursos->findAll(),
+            'titulo' => 'Lista de cursos',
+        ]);
+    }
+}
